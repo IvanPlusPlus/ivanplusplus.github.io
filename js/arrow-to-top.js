bbfbfdf4762b21824774b43554da8790ba201875
IvanPlusPlus <lightclub@yandex.ru>
2019-01-29 18:01:45 +0400

    changecolor

--- js/arrow-to-top.js
+++ js/arrow-to-top.js
@@ -1,4 +1,11 @@
-
+//header sticky
+$(document).on('scroll',function(){
+   if($(this).scrollTop() > 1){
+       $('header').addClass('sticky');
+   } else {
+       $('header').removeClass('sticky');
+   }
+});
 
 
 //below this used for arrow-to-top
@@ -6,25 +13,25 @@
 (function () {
     var link,
         toggleScrollToTopLink = function () {
-            
+
             if ($("body").scrollTop() > 100 || $("html").scrollTop() > 100) {
-                
+
                 link.fadeIn(700);
             } else {
-                
+
                 link.fadeOut(700);
             }
-            
+
         };
-    
+
     $(document).ready(function () {
-        
+
         link = $(".scroll-to-top");
-        
+
         $(window).scroll(toggleScrollToTopLink);
-        
+
         toggleScrollToTopLink();
-        
+
         link.on("click", function () {
             $('body, html').animate({scrollTop: 0},700);
         });
